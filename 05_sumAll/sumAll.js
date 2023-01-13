828b6e1281f69671be7968b381a75b32e9b7a835
Sheharyar <sheharyar.tariq44@gmail.com>
2023-01-13 05:37:36 +0500

    Exercise no. 5 sol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let sum=0;
 
+if(typeof a === 'number' && typeof b === 'number') {
+
+
+if(a <0 || b <0){
+    sum = "ERROR"
+}
+
+else if(b>a){
+
+for(i=a; i<=b ; i++){
+    sum=sum + i; 
+}}
+
+else{
+
+    for(i=b; i<=a; i++){
+        sum=sum + i;
+    }
+
+}}
+
+else {return sum = "ERROR";}
+
+return sum;
 };
-
+sumAll(123,1)
 // Do not edit below this line
 module.exports = sumAll;
