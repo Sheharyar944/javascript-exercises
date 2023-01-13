6c79c0306e4dfc13ab14a9362343641cea727722
Sheharyar <sheharyar.tariq44@gmail.com>
2023-01-13 23:36:15 +0500

    exercise 4 solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,48 @@
-const removeFromArray = function() {
+// const removeFromArray = function() {
     
     
-    let array = arguments[0];
+//     let array = arguments[0];
 
-    for(let i=1; i<arguments.length; i++){
-        for(let j=0; j<array.length; j++){
-        if (arguments[i] === array[j]) { 
-             x = array.splice(j,1);}
-            }
-    }
-    return array;
+//     for(let i=1; i<arguments.length; i++){
+//         for(let j=0; j<array.length; j++){
+//         if (arguments[i] === array[j]) { 
+//              x = array.splice(j,1);}
+//             }
+//     }
+//     return array;
+// };
+
+
+// const removeFromArray = function(...arg) {
+    
+//     let array = arg[0];
+// let newarray = [];
+//     array.forEach(element => {
+
+//         if(!arg.includes(element)) {
+// newarray.push(element);
+//         }
+        
+//     });
+  
+//     return newarray;
+// };
+
+const removeFromArray = function(...arg) {
+    
+let arr=arg[0];
+return arr.filter(element => (!arg.includes(element)));
+
+   
 };
-removeFromArray()
+
+
+
+
+
+
+
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
